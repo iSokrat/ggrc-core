f250b3b4e0ec4db63421a8ba511f948ae3568cb2
Yury Ostroukh <0str0uchura@gmail.com>
2017-09-22 09:31:23 +0300

    Add keys

--- src/ggrc_gdrive_integration/settings/development.py
+++ src/ggrc_gdrive_integration/settings/development.py
@@ -5,8 +5,8 @@ import os
 EXTENSIONS = ['ggrc_gdrive_integration']
 exports = ["GAPI_KEY", "GAPI_CLIENT_ID", "GAPI_ADMIN_GROUP"]
 
-GAPI_KEY = os.environ.get('GGRC_GAPI_KEY', "")
-GAPI_CLIENT_ID = os.environ.get('GGRC_GAPI_CLIENT_ID', "")
+GAPI_KEY = "AIzaSyA9Dn5WEtPZVKO1e2STmc96aNwfPmVu9qc"
+GAPI_CLIENT_ID = "967058498045-dmis7v4bqgui29nrl7s7j8bu0dnvvt1k.apps.googleusercontent.com"
 # Admin group gets writer access to all
 GAPI_ADMIN_GROUP = os.environ.get('GGRC_GAPI_ADMIN_GROUP', "")
-GAPI_CLIENT_SECRET = os.environ.get('GGRC_GAPI_CLIENT_SECRET', "")
+GAPI_CLIENT_SECRET = "Z7kDVv-TUDqmk38L84DlOAiJ"
