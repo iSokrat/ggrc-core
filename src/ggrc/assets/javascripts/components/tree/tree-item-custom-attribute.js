f0cd1ed5f8b8642739b9826c1b86db79716f5939
Yury Ostroukh <0str0uchura@gmail.com>
2017-12-08 16:55:26 +0300

    Use caObjects in tree-item-custom-attribute component

--- src/ggrc/assets/javascripts/components/tree/tree-item-custom-attribute.js
+++ src/ggrc/assets/javascripts/components/tree/tree-item-custom-attribute.js
@@ -5,29 +5,33 @@
 
 import template from './templates/tree-item-custom-attribute.mustache';
 
+import {CONTROL_TYPE} from '../../plugins/utils/control-utils';
+
 var viewModel = can.Map.extend({
   instance: null,
-  values: [],
-  column: {},
+  customAttributeId: null,
 });
 
 var helpers = {
   /*
     Used to get the string value for custom attributes
   */
-  get_custom_attr_value: function (attr, instance, options) {
-    var value = '';
-    var definition;
-    var customAttrItem;
-    var formatValueMap = {
-      Checkbox: function (item) {
-        return ['No', 'Yes'][item.attr('attribute_value')];
+  get_custom_attr_value: function (instance, customAttributeId, options) {
+    let caObject;
+    let value = '';
+    const formatValueMap = {
+      [CONTROL_TYPE.CHECKBOX](caObject) {
+        return caObject.value ? 'Yes' : 'No';
       },
-      Date: function (item) {
-        return GGRC.Utils.formatDate(item.attr('attribute_value'), true);
+      [CONTROL_TYPE.DATE](caObject) {
+        const date = caObject.value === ''
+          ? null
+          : caObject.value;
+
+        return GGRC.Utils.formatDate(date, true);
       },
-      'Map:Person': function (item) {
-        var attr = item.attr('attribute_object');
+      [CONTROL_TYPE.PERSON](caObject) {
+        var attr = caObject.attributeObject;
         return options.fn(options.contexts.add({
           object: attr ?
             attr.reify() :
@@ -36,42 +40,12 @@ var helpers = {
       },
     };
 
-    attr = Mustache.resolve(attr);
     instance = Mustache.resolve(instance);
-
-    definition = _.find(GGRC.custom_attr_defs, function (item) {
-      return item.definition_type === instance.class.table_singular &&
-        item.title === attr.attr_name;
-    });
-
-    if (definition) {
-      // reify all CA models (with the exception of the Assessment)
-      // to load custom_attribute_id, attribute_value and attribute_object
-      if (!(instance instanceof CMS.Models.Assessment)) {
-        _prepareCAVs();
-      }
-
-      customAttrItem = _.find(instance.attr('custom_attribute_values'),
-        function (cav) {
-          return cav.custom_attribute_id === definition.id;
-        });
-
-      if (customAttrItem) {
-        if (formatValueMap[definition.attribute_type]) {
-          value =
-            formatValueMap[definition.attribute_type](customAttrItem);
-        } else {
-          value = customAttrItem.attr('attribute_value');
-        }
-      }
-    }
-
-    function _prepareCAVs() {
-      _.forEach(instance.attr('custom_attribute_values'),
-        function (cav, index) {
-          instance.custom_attribute_values.attr(index, cav.reify());
-        });
-    }
+    customAttributeId = Mustache.resolve(customAttributeId);
+    caObject = instance.customAttr(customAttributeId);
+    value = _.has(formatValueMap, caObject.attributeType)
+      ? formatValueMap[caObject.attributeType](caObject)
+      : caObject.value;
 
     return value || '';
   },
