8a6dc711e80de163091fe67c8d1d2ee794a737cc
Yury Ostroukh <0str0uchura@gmail.com>
2018-01-15 18:18:55 +0300

    Add test for LCA validation actions

--- src/ggrc/assets/javascripts/plugins/utils/custom-attribute/tests/custom-attribute-help-utils_spec.js
+++ src/ggrc/assets/javascripts/plugins/utils/custom-attribute/tests/custom-attribute-help-utils_spec.js
@@ -3,7 +3,11 @@
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-import {hasEmptyValue} from '../custom-attribute-help-utils';
+import {
+  hasEmptyValue,
+  isRequireAttachments,
+  hasMissingEvidence,
+} from '../custom-attribute-help-utils';
 import CustomAttributeObject from '../custom-attribute-object';
 
 describe('hasEmptyValue() function', () => {
@@ -53,5 +57,83 @@ describe('hasEmptyValue() function', () => {
     caObject.value = 12345;
     result = hasEmptyValue(caObject);
     expect(result).toBe(false);
+  });
+});
+
+describe('isRequireAttachments() function', () => {
+  let caObject;
+  let caDef;
+
+  beforeEach(function () {
+    const instance = new can.Map();
+    caDef = new can.Map();
+    caObject = new CustomAttributeObject(instance, caDef);
+  });
+
+  describe('returns true', () => {
+    it('if caObject requires comment', function () {
+      const commentRequired = '1';
+      const optionName = 'Option name';
+      let result;
+
+      caObject.attachedComment = false;
+      caDef.attr('multi_choice_mandatory', commentRequired);
+      caDef.attr('multi_choice_options', optionName);
+
+      result = isRequireAttachments(caObject, optionName);
+      expect(result).toBe(true);
+    });
+
+    it('if caObject requires evidence', function () {
+      const evidenceRequired = '2';
+      const optionName = 'Option name';
+      let result;
+
+      caObject.attachedComment = false;
+      caDef.attr('multi_choice_mandatory', evidenceRequired);
+      caDef.attr('multi_choice_options', optionName);
+
+      result = isRequireAttachments(caObject, optionName);
+      expect(result).toBe(true);
+    });
+  });
+
+  it('returns false by default', function () {
+    const optionName = 'Option name';
+    const result = isRequireAttachments(caObject, optionName);
+    expect(result).toBe(false);
+  });
+});
+
+fdescribe('hasMissingEvidence() function', () => {
+  let caObject;
+  let caDef;
+
+  beforeEach(function () {
+    const instance = new can.Map();
+    caDef = new can.Map();
+    caObject = new CustomAttributeObject(instance, caDef);
+  });
+
+  describe('when evidence is required', () => {
+    beforeEach(function () {
+      const evidenceRequired = '2';
+      const optionName = 'Option name';
+
+      caObject.attachedComment = false;
+      caDef.attr('multi_choice_mandatory', evidenceRequired);
+      caDef.attr('multi_choice_options', optionName);
+    });
+
+    it('...', function () {
+
+    });
+  });
+
+  describe('when evidence is not required', () => {
+    it('returns false', function () {
+      const result = hasMissingEvidence(caObject, '');
+      expect(result).toBe(false);
+    });
   });
 });
