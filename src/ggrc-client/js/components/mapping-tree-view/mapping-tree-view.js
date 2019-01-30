c42d9375a8bf5746502193e9e5257a175ae840f0
Yury Ostroukh <0str0uchura@gmail.com>
2019-01-30 16:09:08 +0300

    Cleanup mapping-tree-view component

--- src/ggrc-client/js/components/mapping-tree-view/mapping-tree-view.js
+++ src/ggrc-client/js/components/mapping-tree-view/mapping-tree-view.js
@@ -4,7 +4,6 @@
 */
 
 import template from './mapping-tree-view.mustache';
-import '../object-list-item/editable-document-object-list-item';
 import Mappings from '../../models/mappers/mappings';
 
 export default can.Component.extend({
@@ -12,67 +11,23 @@ export default can.Component.extend({
   template,
   viewModel: {
     treeViewClass: '@',
-    expandable: '@',
-    sortField: '@',
-    sortOrder: '@',
-    emptyText: '@',
     parentInstance: null,
     mappedObjects: [],
-    isExpandable: function () {
-      let expandable = this.attr('expandable');
-      if (expandable === null || expandable === undefined) {
-        return true;
-      } else if (typeof expandable === 'string') {
-        return expandable === 'true';
-      }
-      return expandable;
-    },
   },
-  init: function (element) {
-    let el = $(element);
-    let binding;
-
+  init(element) {
     _.forEach(['mapping', 'itemTemplate'], (prop) => {
-      if (!this.viewModel.attr(prop)) {
-        this.viewModel.attr(prop,
-          el.attr(can.camelCaseToDashCase(prop)));
-      }
+      this.viewModel.attr(prop,
+        $(element).attr(can.camelCaseToDashCase(prop))
+      );
     });
 
-    binding = Mappings.getBinding(
-      this.viewModel.mapping,
-      this.viewModel.parentInstance);
-
-    binding.refresh_instances().then(function (mappedObjects) {
-      this.viewModel.attr('mappedObjects').replace(
-        this._sortObjects(mappedObjects)
-      );
-    }.bind(this));
-
-    // We are tracking binding changes, so mapped items update accordingly
-    binding.list.on('change', function () {
-      this.viewModel.attr('mappedObjects').replace(
-        this._sortObjects(binding.list)
-      );
-    }.bind(this));
-  },
-  /**
-    * Sort objects list by this.viewModel.sortField, if defined
-    * in order defined in this.viewModel.sortOrder (asc or desc)
-    *
-    * @param {Array} mappedObjects - the list of objects to be sorted
-    *
-    * @return {Array} - if this.viewModel.sortField is defined, mappedObjects
-    *                   sorted by field this field;
-    *                   if this.viewModel.sortField is undefined, unsorted
-    *                   mappedObjects.
-    */
-  _sortObjects: function (mappedObjects) {
-    let sortField = this.viewModel.attr('sortField');
-    let sortOrder = this.viewModel.attr('sortOrder');
-    if (sortField) {
-      return _.orderBy(mappedObjects, sortField, sortOrder);
-    }
-    return mappedObjects;
+    Mappings
+      .getBinding(
+        this.viewModel.mapping,
+        this.viewModel.parentInstance
+      ).refresh_instances()
+      .then((mappedObjects) => {
+        this.viewModel.attr('mappedObjects').replace(mappedObjects);
+      });
   },
 });
