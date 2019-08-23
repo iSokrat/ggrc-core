c166a2fe002c045d8a0336693cc4f054a8d8e75c
pavelglebov <pg.incloud@gmail.com>
2019-08-23 18:28:42 +0300

    Add tests for Download modal

--- src/ggrc-client/js/components/import-export/download-template/download-template_spec.js
+++ src/ggrc-client/js/components/import-export/download-template/download-template_spec.js
@@ -16,7 +16,34 @@ describe('download-template component', () => {
     vm = getComponentVM(Component);
   });
 
-  describe('selectItems method', () => {
+  describe('showExtraFields property', () => {
+    it('should be truthy when Assessments are selected', () => {
+      vm.attr('selected', [{
+        name: 'Assessment',
+      }]);
+      expect(vm.attr('showExtraFields')).toBe(true);
+
+      vm.attr('selected', [{
+        name: 'Program',
+      }, {
+        name: 'Assessment',
+      }, {
+        name: 'Audit',
+      }]);
+      expect(vm.attr('showExtraFields')).toBe(true);
+    });
+
+    it('should be falsy when Assessments are not selected', () => {
+      vm.attr('selected', [{
+        name: 'Program',
+      }, {
+        name: 'AssessmentTemplate',
+      }]);
+      expect(vm.attr('showExtraFields')).toBe(false);
+    });
+  });
+
+  describe('selectItems() method', () => {
     it('should fill selected array from event', () => {
       vm.selectItems({
         selected: [{name: 'Foo'}, {name: 'Bar'}, {name: 'Baz'}],
@@ -47,7 +74,7 @@ describe('download-template component', () => {
     });
   });
 
-  describe('downloadCSV method', () => {
+  describe('downloadCSV() method', () => {
     it('should do ajax call in proper format', (done) => {
       spyOn(Utils, 'downloadTemplate')
         .and.returnValue($.Deferred().resolve('FooBar'));
@@ -93,7 +120,7 @@ describe('download-template component', () => {
     });
   });
 
-  describe('downloadSheet method', () => {
+  describe('downloadSheet() method', () => {
     let downloadSpy;
 
     beforeEach(() => {
@@ -143,6 +170,127 @@ describe('download-template component', () => {
 
         done();
       });
+    });
+  });
+
+  describe('addTemplate() method', () => {
+    it('should add an empty template', () => {
+      vm.attr('templates', []);
+      vm.addTemplate();
+
+      const templates = vm.attr('templates');
+
+      expect(templates[0].serialize()).toEqual({
+        id: null,
+        value: null,
+        isDuplicate: false,
+      });
+    });
+  });
+
+  describe('removeTemplate method', () => {
+    it('should remove particular template', () => {
+      vm.attr('templates', [{
+        id: 1,
+      }, {
+        id: 2,
+      }, {
+        id: 3,
+      }]);
+      vm.removeTemplate(1);
+
+      const templates = vm.attr('templates');
+
+      expect(templates.length).toEqual(2);
+      expect(templates[0].id).toEqual(1);
+      expect(templates[1].id).toEqual(3);
+    });
+
+    it('should call recalculation of duplicates', () => {
+      spyOn(vm, 'calculateDuplicates');
+
+      vm.removeTemplate();
+      expect(vm.calculateDuplicates).toHaveBeenCalled();
+    });
+  });
+
+  describe('selectTemplate() method', () => {
+    it('should select a template', () => {
+      vm.attr('templates', [{
+        id: null,
+        value: null,
+      }]);
+      const selectedItem = {
+        id: 321,
+        title: 'Mock Template',
+      };
+
+      vm.selectTemplate(selectedItem, 0);
+
+      const templates = vm.attr('templates');
+
+      expect(templates[0].id).toEqual(321);
+      expect(templates[0].value).toEqual('Mock Template');
+      expect(selectedItem.value).toEqual(selectedItem.title);
+    });
+
+    it('should call recalculation of duplicates', () => {
+      spyOn(vm, 'calculateDuplicates');
+      const selectedItem = {};
+      vm.attr('templates', [{}]);
+
+      vm.selectTemplate(selectedItem, 0);
+      expect(vm.calculateDuplicates).toHaveBeenCalled();
+    });
+  });
+
+  describe('calculateDuplicates() method', () => {
+    it('should mark duplicates', () => {
+      vm.attr('templates', [{
+        id: 91,
+        value: 'Mock91',
+      }, {
+        id: 92,
+        value: 'Mock92',
+      }, {
+        id: 991,
+        value: 'Mock91',
+      }, {
+        id: 91,
+        value: 'Mock91a',
+      }]);
+
+      vm.calculateDuplicates();
+
+      const templates = vm.attr('templates');
+
+      expect(templates[0].isDuplicate).toBe(false);
+      expect(templates[1].isDuplicate).toBe(false);
+      expect(templates[2].isDuplicate).toBe(false);
+      expect(templates[3].isDuplicate).toBe(true);
+    });
+  });
+
+  describe('prepareTemplateIds() method', () => {
+    it('should extract ids and remove duplicates', () => {
+      vm.attr('templates', [{
+        id: 91,
+        value: 'Mock91',
+      }, {
+        id: 92,
+        value: 'Mock92',
+      }, {
+        id: null,
+        value: null,
+      }, {
+        id: null,
+        value: null,
+      }]);
+
+      const preparedObject = {};
+      vm.prepareTemplateIds(preparedObject);
+
+      expect(preparedObject.template_ids.serialize()).toEqual([91, 92]);
     });
   });
 });
